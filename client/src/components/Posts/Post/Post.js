f25cc20067406e30bdee00bf985ea1ce1a41ab2f
Dimitar.Petroww <Mitko66692@abv.bg>
2021-08-22 16:51:10 +0300

    update

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -4,7 +4,7 @@ import PostAuthor from './PostAuthor/PostAuthor';
 function Post() {
     return (
         <div className="post">
-            <img src="/" alt="" />
+            <img src="/blue-origami-bird.png" alt="" />
             <p className="description">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nostrum quo fuga! Quaerat laboriosam commodi doloribus, earum, reiciendis amet dolorum odit fugit eligendi molestias cupiditate, explicabo iste natus numquam ab.
             </p>
