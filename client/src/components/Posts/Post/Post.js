d9519dedfd056ce6ebec450e11353d489624c9ef
Dimitar.Petroww <Mitko66692@abv.bg>
2021-08-22 16:46:37 +0300

    add Post

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,19 @@
+import './Post.css';
+
+function Post() {
+    return (
+        <div className="post">
+            <img src="" alt="" />
+            <p className="description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nostrum quo fuga! Quaerat laboriosam commodi doloribus, earum, reiciendis amet dolorum odit fugit eligendi molestias cupiditate, explicabo iste natus numquam ab.
+            </p>
+            <div>
+                <span>
+                    <small>Author:</small> qnko
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default Post;
